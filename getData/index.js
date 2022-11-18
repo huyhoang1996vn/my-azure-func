51e18737b99fb5051c0a0c091735189f5e4f0953
steve <steve.nguyen@linksinternational.com>
2022-11-18 17:04:09 +0700

    Durable function

--- getData/index.js
+++ getData/index.js
@@ -1,8 +1,9 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+    var moment = require('moment');
+    
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = "responseMessage"
+    const responseMessage = `responseMessage ${moment.utc().valueOf()}`
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
